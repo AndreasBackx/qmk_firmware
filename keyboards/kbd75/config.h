57ced3fc01a5bdcf81bc725b814968470869f714
Andreas Backx <andreas@backx.org>
2017-08-31 00:11:41 +0200

    Added rgblight features to my layout.

--- keyboards/kbd75/config.h
+++ keyboards/kbd75/config.h
@@ -50,9 +50,9 @@
 #ifdef RGB_DI_PIN
 #define RGBLIGHT_ANIMATIONS
 #define RGBLED_NUM 16
-#define RGBLIGHT_HUE_STEP 8
-#define RGBLIGHT_SAT_STEP 8
-#define RGBLIGHT_VAL_STEP 8
+#define RGBLIGHT_HUE_STEP 32
+#define RGBLIGHT_SAT_STEP 32
+#define RGBLIGHT_VAL_STEP 32
 #endif
 
 #endif
--- keyboards/kbd75/keymaps/andreas/keymap.c
+++ keyboards/kbd75/keymaps/andreas/keymap.c
@@ -9,23 +9,23 @@
 const uint16_t PROGMEM keymaps[][MATRIX_ROWS][MATRIX_COLS] = {
   /* Keymap _BL: (Base Layer) Default Layer
    * ,----------------------------------------------------------------.
-   * |Esc | 1|  2|  3|  4|  5|  6|  7|  8|  9|  0|  -|  =|Backsp |~ ` |
+   * |Esc |F1|F 2|F 3|F 4|F 5|F 6|F 7|F 8|F 9|F10|F11|F12|Prt|Fn |Del |
    * |----------------------------------------------------------------|
    * |~ ` | 1|  2|  3|  4|  5|  6|  7|  8|  9|  0|  -|  =|Backsp |Home|
    * |----------------------------------------------------------------|
-   * |Tab  |  Q|  W|  E|  R|  T|  Y|  U|  I|  O|  P|  [|  ]|  \  |Del |
+   * |Tab  |  Q|  W|  E|  R|  T|  Y|  U|  I|  O|  P|  [|  ]|  \  |PgUp|
    * |----------------------------------------------------------------|
-   * |CAPS   |  A|  S|  D|  F|  G|  H|  J|  K|  L|  ;|  '|Return |PgUp|
+   * |CAPS   |  A|  S|  D|  F|  G|  H|  J|  K|  L|  ;|  '|Return |PgDn|
    * |----------------------------------------------------------------|
-   * |Shift   |  Z|  X|  C|  V|  B|  N|  M|  ,|  .|  /|Shift | Up|PgDn|
+   * |Shift   |  Z|  X|  C|  V|  B|  N|  M|  ,|  .|  /|Shift | Up|End |
    * |----------------------------------------------------------------|
    * |Ctrl|Alt |Sup |        Space          | FN|Alt|Ctrl|Lef|Dow|Rig |
    * `----------------------------------------------------------------'
    */
   // KC_BSPC
 [_BL] = KEYMAP(
-  KC_ESC,    KC_F1,   KC_F2,  KC_F3,  KC_F4,  KC_F5,  KC_F6,  KC_F7,   KC_F8,  KC_F9,  KC_F10,  KC_F11,  KC_F12, KC_PSCR,   MO(1), KC_HOME,
-  KC_GRV,     KC_1,    KC_2,   KC_3,   KC_4,   KC_5,   KC_6,   KC_7,    KC_8,   KC_9,    KC_0, KC_MINS,  KC_EQL, KC_DEL,  KC_BSPC,  KC_DEL, \
+  KC_ESC,    KC_F1,   KC_F2,  KC_F3,  KC_F4,  KC_F5,  KC_F6,  KC_F7,   KC_F8,  KC_F9,  KC_F10,  KC_F11,  KC_F12, KC_PSCR,   MO(1),  KC_DEL,
+  KC_GRV,     KC_1,    KC_2,   KC_3,   KC_4,   KC_5,   KC_6,   KC_7,    KC_8,   KC_9,    KC_0, KC_MINS,  KC_EQL, KC_DEL,  KC_BSPC, KC_HOME, \
   KC_TAB,     KC_Q,    KC_W,   KC_E,   KC_R,   KC_T,   KC_Y,   KC_U,    KC_I,   KC_O,    KC_P, KC_LBRC, KC_RBRC,          KC_BSLS, KC_PGUP, \
   KC_CAPS,    KC_A,    KC_S,   KC_D,   KC_F,   KC_G,   KC_H,   KC_J,    KC_K,   KC_L, KC_SCLN, KC_QUOT,                    KC_ENT, KC_PGDN, \
   KC_LSFT, _______,    KC_Z,   KC_X,   KC_C,   KC_V,   KC_B,   KC_N,   KC_M, KC_COMM,  KC_DOT, KC_SLSH,           KC_RSFT,   KC_UP, KC_END, \
@@ -49,10 +49,19 @@ const uint16_t PROGMEM keymaps[][MATRIX_ROWS][MATRIX_COLS] = {
 [_FL] = KEYMAP(
   KC_SYSTEM_SLEEP, _______, _______, _______, _______, _______, _______, _______, _______, _______, _______, _______, _______, _______, _______, _______,
           _______, _______, _______, _______, _______, _______, _______, _______, _______, _______, _______, _______, _______, _______,  KC_DEL, _______, \
-            RESET, _______,   KC_UP, _______, _______, _______, _______, _______, _______, _______, _______, _______, _______,          _______, BL_TOGG, \
-          _______, KC_LEFT, KC_DOWN,KC_RIGHT, _______, _______, _______, _______, _______, _______, _______, _______,                   KC_MPLY,  BL_INC, \
-          _______, _______, _______, _______, _______, _______, _______, _______, _______, _______, _______, _______,          _______, KC_VOLU,  BL_DEC, \
+            RESET, RGB_HUD, RGB_VAI, RGB_HUI, _______, _______, _______, _______, _______, _______, _______, _______, _______,          _______, BL_TOGG, \
+          RGB_TOG, RGB_SAD, RGB_VAD, RGB_SAI, RGB_MOD, _______, _______, _______, _______, _______, _______, _______,                   KC_MPLY, _______, \
+          _______, _______, _______, _______, _______, _______, _______, _______, _______, _______, _______, _______,          _______, KC_VOLU, _______, \
           _______, _______, _______,          _______, _______, _______,                            _______, _______, _______, KC_MPRV, KC_VOLD, KC_MNXT),
+};
+
+/**
+ * Startup commands.
+ */
+void matrix_init_user(void) {
+  rgblight_enable();
+  rgblight_mode(1);  // Solid colour
+  rgblight_sethsv(43, 255, 255);  // Yellow
 };
 
 static bool control_disabled = false;
@@ -88,92 +97,12 @@ bool process_record_kb(uint16_t keycode, keyrecord_t *record) {
     register_code(KC_BSPC);
     return false;
   }
+
+  if(keycode == RESET) {
+    rgblight_enable();
+    rgblight_mode(1);
+    rgblight_sethsv(0, 255, 255);  // Red
+  }
+
   return true;
-}
-
-const macro_t *action_get_macro(keyrecord_t *record, uint8_t id, uint8_t opt) {
-
-
-  switch (id) {
-
-  }
-  return MACRO_NONE;
-}
-
-void matrix_init_user(void) {
-}
-
-void matrix_scan_user(void) {
-}
-
-bool process_record_user(uint16_t keycode, keyrecord_t *record) {
-  return true;
-}
-
-void led_set_user(uint8_t usb_led) {
-
-  if (usb_led & (1 << USB_LED_NUM_LOCK)) {
-
-  } else {
-
-  }
-
-  if (usb_led & (1 << USB_LED_CAPS_LOCK)) {
-    DDRB |= (1 << 2); PORTB &= ~(1 << 2);
-  } else {
-    DDRB &= ~(1 << 2); PORTB &= ~(1 << 2);
-  }
-
-  if (usb_led & (1 << USB_LED_SCROLL_LOCK)) {
-
-  } else {
-
-  }
-
-  if (usb_led & (1 << USB_LED_COMPOSE)) {
-
-  } else {
-
-  }
-
-  if (usb_led & (1 << USB_LED_KANA)) {
-
-  } else {
-
-  }
-
-}
-
-enum function_id {
-    SHIFT_ESC,
-};
-
-const uint16_t PROGMEM fn_actions[] = {
-  [0]  = ACTION_FUNCTION(SHIFT_ESC),
-};
-
-void action_function(keyrecord_t *record, uint8_t id, uint8_t opt) {
-  static uint8_t shift_esc_shift_mask;
-  switch (id) {
-    case SHIFT_ESC:
-      shift_esc_shift_mask = get_mods()&MODS_CTRL_MASK;
-      if (record->event.pressed) {
-        if (shift_esc_shift_mask) {
-          add_key(KC_GRV);
-          send_keyboard_report();
-        } else {
-          add_key(KC_ESC);
-          send_keyboard_report();
-        }
-      } else {
-        if (shift_esc_shift_mask) {
-          del_key(KC_GRV);
-          send_keyboard_report();
-        } else {
-          del_key(KC_ESC);
-          send_keyboard_report();
-        }
-      }
-      break;
-  }
 }
